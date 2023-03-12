6bdc6b6ff431435cee1d4032cba5b370cebc9cf5
Drabbity <drabbity@protonmail.com>
2023-03-12 16:42:18 +0400

    FullTask

---
+++ SoftgenTask/Scripts/AcademicDatabase.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using SoftgenTask.Enum;
+
+namespace SoftgenTask
+{
+    public static class AcademicDatabase
+    {
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["SoftgenTask.Properties.Settings.MainDatabaseConnectionString"].ConnectionString;
+
+        public static Academic GetUser(AcademicTable table, int id)
+        {
+            string query = $"SELECT * FROM { AcademicTableString(table) } WHERE Id = { id }";
+
+            Academic academic = new Academic();
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        academic.Id = reader.GetInt32(0);
+                        academic.FirstName = reader.GetString(1);
+                        academic.LastName = reader.GetString(2);
+                        academic.EmailAddress = reader.GetString(3);
+                        academic.BirthDate = reader.GetDateTime(4);
+                    }
+                }
+            }
+            return academic;
+        }
+
+
+        public static void AddUser(AcademicTable table, string firstName, string lastName, string emailAddress, DateTime birthDate)
+        {
+            string query = $"INSERT INTO { AcademicTableString(table) } VALUES (@FirstName, @LastName, @EmailAddress, @BirthDate)";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@EmailAddress", emailAddress);
+                command.Parameters.AddWithValue("@BirthDate", birthDate);
+
+                command.ExecuteScalar();
+            }
+        }
+
+        public static List<int> SearchUser(AcademicTable table, string firstName = "", string lastName = "", string emailAddress = "", DateTime? birthDate = null)
+        {
+            string query = $"SELECT Id FROM { AcademicTableString(table) } "
+                + $"WHERE FirstName LIKE '%{firstName}%' "
+                + $"AND LastName LIKE '%{lastName}%' ";
+
+            if (emailAddress != "")
+                query += $"AND Email = '{emailAddress}' ";
+
+            if (birthDate != null)
+                query += $"AND BirthDate = '{birthDate}' ";
+
+            var idList = new List<int>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            idList.Add(reader.GetInt32(i));
+                        }
+                    }
+                }
+            }
+
+            return idList;
+        }
+
+        public static bool EditUser(AcademicTable table, int id, string firstName = "", string lastName = "", string emailAddress = "", DateTime? birthDate = null)
+        {
+            bool isValid = false;
+
+            string query = $"UPDATE { AcademicTableString(table) } SET";
+
+            if (firstName != "")
+            {
+                query += $" FirstName = '{ firstName }',";
+                isValid = true;
+            }
+            if (lastName != "")
+            {
+                query += $" LastName = '{ lastName }',";
+                isValid = true;
+            }
+            if (emailAddress != "")
+            {
+                query += $" Email = '{ emailAddress }',";
+                isValid = true;
+            }
+            if (birthDate != null)
+            {
+                query += $" BirthDate = '{ birthDate }',";
+                isValid = true;
+            }
+
+            query = query.Remove(query.Length - 1);
+            query += $" WHERE Id = { id }";
+
+            if (isValid)
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                using (var command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    command.ExecuteScalar();
+                }
+            }
+            return isValid;
+        }
+
+        public static void DeleteUser(AcademicTable table, int id)
+        {
+            string query = $"DELETE FROM { AcademicTableString(table) } WHERE id = { id }";
+
+            DeleteUsersFromGroups(table, id);
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+
+        private static void DeleteUsersFromGroups(AcademicTable table, int id)
+        {
+            string query = $"DELETE FROM { AcademicGroupTableString(table) } WHERE AcademicId = { id }";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+
+        private static string AcademicTableString(AcademicTable table)
+        {
+            switch(table)
+            {
+                case AcademicTable.STUDENTS:
+                    return "Students";
+                case AcademicTable.TEACHERS:
+                    return "Teachers";
+                default:
+                    throw new NotImplementedException($"Code not implemented for academic Table: { table }");
+            }
+        }
+
+        private static string AcademicGroupTableString(AcademicTable table)
+        {
+            switch (table)
+            {
+                case AcademicTable.STUDENTS:
+                    return "StudentGroup";
+                case AcademicTable.TEACHERS:
+                    return "TeacherGroup";
+                default:
+                    throw new NotImplementedException($"Code not implemented for academic Table: { table }");
+            }
+        }
+    }
+}
---
+++ SoftgenTask/Scripts/GroupDatabase.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftgenTask
+{
+    public static class GroupDatabase
+    {
+        private const string _GROUP_TABLE_NAME = "Groups";
+        private const string _GROUP_STUDENT_TABLE_NAME = "StudentGroup";
+        private const string _GROUP_TEACHER_TABLE_NAME = "TeacherGroup";
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["SoftgenTask.Properties.Settings.MainDatabaseConnectionString"].ConnectionString;
+
+        public static void AddGroup(string groupName)
+        {
+            string query = $"INSERT INTO { _GROUP_TABLE_NAME } VALUES ('{ groupName }')";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+
+        public static void AddStudent(int groupId, int studentId)
+            => AddAcademic(_GROUP_STUDENT_TABLE_NAME, groupId, studentId);
+
+        public static void AddTeacher(int groupId, int teacherId)
+            => AddAcademic(_GROUP_TEACHER_TABLE_NAME, groupId, teacherId);
+
+        private static void AddAcademic(string table, int groupId, int academicId)
+        {
+            string query = $"INSERT INTO { table } VALUES ( @groupId, @academicId )";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.Parameters.AddWithValue("@groupId", groupId);
+                command.Parameters.AddWithValue("@academicId", academicId);
+
+                command.ExecuteScalar();
+            }
+        }
+
+        public static void RemoveStudent(int groupId, int studentId)
+            => RemoveAcademic(_GROUP_STUDENT_TABLE_NAME, groupId, studentId);
+
+        public static void RemoveTeacher(int groupId, int studentId)
+            => RemoveAcademic(_GROUP_TEACHER_TABLE_NAME, groupId, studentId);
+
+        private static void RemoveAcademic(string table, int groupId, int academicId)
+        {
+            string query = $"DELETE FROM { table } WHERE GroupId = { groupId } AND AcademicId = { academicId }";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+        public static List<int> GetStudents(int groupId)
+            => GetAcademics(_GROUP_STUDENT_TABLE_NAME, groupId);
+
+        public static List<int> GetTeachers(int groupId)
+            => GetAcademics(_GROUP_TEACHER_TABLE_NAME, groupId);
+
+        private static List<int> GetAcademics(string table, int groupId)
+        {
+            string query = $"SELECT AcademicId FROM { table } WHERE GroupId = { groupId }";
+
+            var idList = new List<int>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            idList.Add(reader.GetInt32(i));
+                        }
+                    }
+                }
+            }
+
+            return idList;
+        }
+
+        public static List<int> SearchGroup(string groupName)
+        {
+            string query = $"SELECT Id FROM { _GROUP_TABLE_NAME } WHERE Name LIKE '%{ groupName }%'";
+
+            var idList = new List<int>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            idList.Add(reader.GetInt32(i));
+                        }
+                    }
+                }
+            }
+
+            return idList;
+        }
+
+        public static void EditName(int id, string newName)
+        {
+
+            string query = $"UPDATE { _GROUP_TABLE_NAME } SET Name = '{ newName }' WHERE Id = { id }";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+
+        public static void DeleteGroup(int id)
+        {
+            string query = $"DELETE FROM { _GROUP_TABLE_NAME } WHERE id = { id }";
+
+            DeleteGroupsFromCombinedTables(_GROUP_STUDENT_TABLE_NAME, id);
+            DeleteGroupsFromCombinedTables(_GROUP_TEACHER_TABLE_NAME, id);
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+
+        private static void DeleteGroupsFromCombinedTables(string tableName, int id)
+        {
+            string query = $"DELETE FROM { tableName } WHERE GroupId = { id }";
+
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                command.ExecuteScalar();
+            }
+        }
+    }
+}
